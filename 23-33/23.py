20449adeb542303e4ddc92ece27de305abee57cc
wm2242 <z180640@outlook.com>
2026-06-16 04:30:40 +0800

    修改:改变23和40的路径,使其适应网页

--- 23-33/23.py
+++ 23-33/23.py
@@ -1,14 +1,14 @@
 import os
 def main():
-    with open('23/students.txt', 'r') as file:
+    with open('23-33/23/students.txt', 'r') as file:
         file.readline()
         while True:
             line = file.readline()
             if line == "":
                 break
             name = line.split()[0] + line.split()[1]
-            if not os.path.exists(f"23//{name}"):
-                os.mkdir(f"23//{name}")
+            if not os.path.exists(f"23-33/23/{name}"):
+                os.mkdir(f"23-33/23/{name}")
                 print(f"已创建: {name}")
             else:
                 print(f"目录已存在，跳过: {name}")
